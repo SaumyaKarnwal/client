883118de96c24a4deea0bee4e55db547fef0e1a3
SaumyaKarnwal <sau.kar.03@gmail.com>
2023-10-10 23:31:17 +0530

    slider

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,21 +2,62 @@ import { Fragment} from "react";
 import { styled, Box } from "@mui/material";
 import Banner from "./Banner";
 import NavBar from "./NavBar"; 
+import { useEffect } from "react";
+import { getProducts as listProducts } from "../../redux/actions/productActions";
+import {useDispatch, useSelector } from 'react-redux';
+import Slide from "./Slide";
+import MidSection from "./MidSection";
+import MidSlide from "./MidSlide";
 
 const Component = styled(Box)`
 padding:10px 5px;
 background: #f2f2f2;
 `
 
-const Home =() => {
+const Home = () => {
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, error } = getProducts;
+
+    // const getProducts = useSelector(state => state.getProducts)
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(listProducts())
+    },[dispatch])
+
     return (
         <>
-            <NavBar/>
+            <NavBar />
             <Component>
-                <Banner/>
+                <Banner />
+                <MidSlide products={products} />
+                <MidSection />
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
             </Component>
         </>
-        
     )
 }
 
